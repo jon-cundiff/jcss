6f6d2215bf46c935b5726f7675dbab4bb68fc244
jon-cundiff <jon.cundiff@gmail.com>
2022-03-16 16:19:51 -0500

    implements dropdown styling. changes default colors

--- colorGenerator.js
+++ colorGenerator.js
@@ -3,11 +3,11 @@ const fs = require("fs");
 
 const buildCSS = (keys) => {
     const colorKeys = {
-        primary: keys ? keys.primary || "#6e41e2" : "#6e41e2",
-        secondary: keys ? keys.secondary || "#2daf93" : "#2daf93",
-        info: keys ? keys.info || "#428bf9" : "#428bf9",
-        danger: keys ? keys.danger || "#C7302B" : "#C7302B",
-        success: keys ? keys.success || "#27AE60" : "#27AE60",
+        primary: keys ? keys.primary || "#0000bb" : "#0000bb",
+        secondary: keys ? keys.secondary || "#b700b7" : "#b700b7",
+        info: keys ? keys.info || "#00bcb0" : "#00bcb0",
+        danger: keys ? keys.danger || "#b20000" : "#b20000",
+        success: keys ? keys.success || "#00bb00" : "#00bb00",
     };
 
     let cssString = ":root {\n";
@@ -20,12 +20,12 @@ const buildCSS = (keys) => {
     for (const [key, value] of Object.entries(colorKeys)) {
         const colorSwatch = chroma.scale(["white", value, "black"]).colors(21);
         appendCssVar(colorSwatch, key, 10);
-        appendCssVar(colorSwatch, key, 4, "lightest");
-        appendCssVar(colorSwatch, key, 8, "lighter");
+        appendCssVar(colorSwatch, key, 3, "lightest");
+        appendCssVar(colorSwatch, key, 5, "lighter");
         appendCssVar(colorSwatch, key, 13, "dark");
     }
     cssString +=
-        "\t--color-white: #fff;\n\t--color-gray-light: #f1f1f1;\n\t--color-gray-medium: #d4d4d4;\n\t--color-gray-dark: #6b6b6b;\n}";
+        "\t--color-white: #fff;\n\t--color-gray-light: #f1f1f1;\n\t--color-gray-medium: #d4d4d4;\n\t--color-gray-dark: #6b6b6b;\n\t--color-gray-darker: #3e3e3e;\n}";
 
     return cssString;
 };
